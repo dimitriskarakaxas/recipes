a6c39c85906aa50cf38f5a18262fb16fd1869a67
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2022-02-09 20:47:48 +0200

    add: mobile friendly on TimeLine

--- src/components/TimeLine/Recipe.js
+++ src/components/TimeLine/Recipe.js
@@ -4,13 +4,13 @@ const Recipe = ({ recipeData }) => {
   console.log(recipeData);
 
   return (
-    <div className="flex w-[200px] p-1 bg-gray-400 rounded">
+    <div className="p-1 bg-gray-300 rounded-lg flex h-full ml-2 w-[170px] flex-shrink-0">
       <img
-        className="object-cover max-w-full w-3/5"
+        className="object-cover max-w-full w-3/5 rounded-lg"
         src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goulash-1640024815.jpeg?crop=1.00xw:0.752xh;0,0.115xh&resize=1200:*"
         alt="Recipe"
       />
-      <div className="w-2/5 text-right">{recipeData.selectedRecipe}</div>
+      <div className="text-sm text-right w-2/5">{recipeData.selectedRecipe}</div>
     </div>
   );
 };
