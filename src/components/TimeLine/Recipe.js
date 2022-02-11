c6172f63afb673523d7b46b75f7556eb64ec97aa
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2022-02-11 18:47:43 +0200

    change: styles

--- src/components/TimeLine/Recipe.js
+++ src/components/TimeLine/Recipe.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Recipe = ({ recipeData }) => {
+const Recipe = ({ recipeData, onShowAddRecipe }) => {
   console.log(recipeData);
 
   return (
-    <div className="p-1 bg-gray-300 rounded-lg flex h-full ml-2 w-[170px] flex-shrink-0 transition-all hover:scale-105">
+    <div
+      className="p-1 bg-gray-300 rounded-lg flex h-full ml-2 w-[170px] flex-shrink-0 transition-all hover:scale-105"
+      onClick={onShowAddRecipe}
+    >
       <img
         className="object-cover max-w-full w-3/5 rounded-lg"
         src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goulash-1640024815.jpeg?crop=1.00xw:0.752xh;0,0.115xh&resize=1200:*"
